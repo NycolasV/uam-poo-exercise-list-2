609166daeaf54e1696ca335b66285e7842d4a25e
NycolasVieiraSN <nvieira@spaceneedle.com.br>
2019-09-17 19:34:45 -0300

    Modularizando o código criado

--- src/main/java/uam/poo/listaexerciciospoo2/views/Main.java
+++ src/main/java/uam/poo/listaexerciciospoo2/views/Main.java
@@ -10,66 +10,90 @@ import uam.poo.listaexerciciospoo2.models.Lampada;
  */
 public class Main {
    public static void main(String[] args){
-       testeIOConstrutor();
+       testeIOConstrutorMod();
    }
    
-   public static void testeIOConstrutor(){
-       try (Scanner input = new Scanner(System.in)) {
-           System.out.println("Insira um preco na lampada: ");
-           float preco  = input.nextFloat();
-           input.nextLine();
-           
-           System.out.println("A lampada esta acesa? ");
-           boolean estaAcessa = input.nextBoolean();
-           input.nextLine();
-           
-           System.out.println("--------------------------------");
-           
-           System.out.println("Insira o id do empregado: ");
-           int id = input.nextInt();
-           input.nextLine();
-           
-           System.out.println("Insira o primeiro nome do empregado: ");
-           String primeiroNome = input.nextLine();
-           
-           System.out.println("Insira o sobrenome do empregado: ");
-           String sobreNome = input.nextLine();
-           
-           System.out.println("Insira o salario do empregado: ");
-           Float salario = input.nextFloat();
-           input.nextLine();
-           
-           System.out.println("--------------------------------");
-           
+   public static void testeIOConstrutorMod(){
+       var input = new Scanner(System.in);
+       
+       insertValuesLampada(input);
+       
+       insertValuesEmpregado(input);
+       
+       insertValuesBotao(input);
+       
+       input.close();
+   }
+   
+   public static Lampada insertValuesLampada(Scanner input){
+       try{
+            System.out.println("Insira um preco na lampada: ");
+            float preco  = input.nextFloat();
+            input.nextLine();
+
+            System.out.println("A lampada esta acesa? ");
+            boolean estaAcessa = input.nextBoolean();
+
+            System.out.println("--------------------------------");
+            return new Lampada(preco, estaAcessa);
+       } catch (Exception ex) {
+           System.out.println(ex.getMessage());
+           return null;
+       }
+   }
+   
+   public static Empregado insertValuesEmpregado(Scanner input){
+       try{
+            System.out.println("Insira o id do empregado: ");
+            int id = input.nextInt();
+            input.nextLine();
+
+            System.out.println("Insira o primeiro nome do empregado: ");
+            String primeiroNome = input.nextLine();
+
+            System.out.println("Insira o sobrenome do empregado: ");
+            String sobreNome = input.nextLine();
+
+            System.out.println("Insira o salario do empregado: ");
+            Float salario = input.nextFloat();
+
+            System.out.println("--------------------------------");
+            return new Empregado(id, primeiroNome, sobreNome, salario);
+       } catch (Exception ex) {
+           System.out.println(ex.getMessage());
+           return null;
+       }
+   }
+   
+   public static Botao insertValuesBotao(Scanner input){
+       try{
            System.out.println("Insira a posicao horizontal do botao: ");
            float posX = input.nextFloat();
            input.nextLine();
-           
+
            System.out.println("Insira a posicao vertical do botao: ");
            float posY = input.nextFloat();
            input.nextLine();
-           
+
            System.out.println("Insira a largura do botao: ");
            float largura = input.nextFloat();
            input.nextLine();
-           
+
            System.out.println("Insira a altura do botao: ");
            float altura = input.nextFloat();
            input.nextLine();
-           
+
            System.out.println("Insira o texto do botao: ");
            String texto = input.nextLine();
-           
+
            System.out.println("Insira o formato do botao: ");
            int formato = input.nextInt();
-           
+
            System.out.println("--------------------------------");
-           
-           Lampada lampada1 = new Lampada(preco, estaAcessa);
-           Empregado empregado1 = new Empregado(id, primeiroNome, sobreNome, salario);
-           Botao botao1 = new Botao(posX, posY, largura, altura, texto, formato);
-       } catch (Exception ex){
+           return new Botao(posX, posY, largura, altura, texto, formato);   
+       } catch (Exception ex) {
            System.out.println(ex.getMessage());
+           return null;
        }
    }
 }
